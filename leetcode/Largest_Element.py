d9020de913cbcd7b80ccb368cb013ebc2617d5a7
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-10-31 02:43:23 +0530

    added largest number

--- leetcode/Largest_Element.py
+++ leetcode/Largest_Element.py
@@ -1,12 +1,35 @@
-array = [707, 108, 595, 649, 123, 698, 6969, 6961]
-length = len(array)
-for i in range(length):
-    min_index = i
-    for j in range(i + 1, length):
-        if array[j] < array[min_index]:
-            min_index = j
-    array[i], array[min_index] = array[min_index], array[i]
+array = [707, 108, 595, 649, 123, 68, 66, 1]
+# length = len(array)
+# for i in range(length):
+#     min_index = i
+#     for j in range(i + 1, length):
+#         if array[j] < array[min_index]:
+#             min_index = j
+#     array[i], array[min_index] = array[min_index], array[i]
     
 
-print("Max element:", array[-1] if array else None)
+# print("Max element:", array[-1] if array else None)
+
+
+# ? 2nd method..............
+
+# largest = array[0]
+# n = len(array)
+# for i in range(n): 
+#     largest = max(largest,array[i])
+# print(largest)
+
+# time_complexity = O(N)
+# space_complexity = O(1)
+
+# ?..........................
+
+# * 3rd method................
+largest = float("-inf")
+n = len(array)
+for i in range(n):
+    largest = max(largest,array[i])
+print( largest)
+
+
 
