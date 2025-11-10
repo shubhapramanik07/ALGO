bfa24ed3121734d5b47f96491cc50d2d8942199f
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-11-11 02:56:05 +0530

    Leetcode 48: Rotate Matrix by 90 Degrees - Part 42

--- leetcode/Leetcode48.py
+++ leetcode/Leetcode48.py
@@ -20,3 +20,29 @@ print("**************************************")
 #* /****************************************/
 #? optimal solution without using extra space   
 # step 1: transpose the matrix
+n = len(matrix)
+for i in range(0,n-1):
+    for j in range(i+1,n):
+        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        # time complexity: O(n*n) because we are traversing half of the matrix
+
+# step 2: reverse each row
+for i in range(n):
+    matrix[i].reverse() 
+    #   time complexity: O(n*n) because we are reversing n rows each of size n
+
+    # here we can also use two pointer approach to reverse the row like below:
+    # left = 0
+    # right = len(matrix[i]) - 1
+    # while left < right:
+    #     matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
+    #     left += 1
+    #     right -= 1
+
+
+
+print(matrix)
+
+
+# time complexity: O(n^2) + O(n^2) = O(n^2) where n is the number of rows or columns in the matrix
+# space complexity: O(1) as we are not using any extra space
